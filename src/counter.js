caac2edd48c03d274fc85b325e5e56c9357196dc
Pranavjoshi8999 <134476623+Pranavjoshi8999@users.noreply.github.com>
2023-10-02 09:47:42 +0530

    first

---
+++ src/counter.js
@@ -0,0 +1,56 @@
+// src/Counter.js
+import React, { useState, useEffect } from 'react';
+
+
+
+
+const ErrorMessage = ({ message }) => {
+  return <p className="error-message">{message}</p>;
+};
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [error, setError] = useState(null); // State to store error message
+
+  useEffect(() => {
+    console.log(`Count changed: ${count}`);
+  }, [count]);
+
+  const increment = () => {
+    setCount(count + 1);
+    setError(null); // Clear any previous errors
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+      setError(null); // Clear any previous errors
+    } else {
+      setError("Error:Cannot go below 0."); // Set error message
+    }
+  };
+
+  const reset = () => {
+    setCount(0);
+    setError(null); // Clear any previous errors
+  };
+
+  return (
+    <div className="counter-container">
+      <h1>Counter using React</h1>
+      
+      <p className='count-class'>Your current count is : {count}</p>
+      {error && <ErrorMessage message={error} />} {/* Display error message if present */}
+      <div className="button-container">
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={increment}>Increment</button>
+        {/* <button onClick={()=>{setCount(0)}}>Decrement</button> */}
+        {count > 10 && (
+          <button className='go' onClick={reset}>GO BACK TO 0</button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
